cde860d5f050a698fc7236eed42cfdf4b1d34b1b
kylevalenzuela <valenzuela.kyle@gmail.com>
2014-05-22 00:34:47 -0700

    Homepage

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Homepage
+ *
+ *
+ * @package Baking Brew
+ */
+
+get_header(); ?>
+
+
+<article class="main-recipe">
+	<div class="main-recipe-img"></div>
+	<div class="main-recipe-block">
+		<h5>Lorem Ipsum</h5>
+		<div class="cellar-info-module">Beer | Brewer</div>
+	</div>
+</article>	
+
+<article class="recent-recipe">
+	<div class="recent-recipe-block">
+		<div class="recent-recipe-article">
+			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+			<div class="cellar-info-module">Beer | Brewer</div>
+		</div>
+		<div class="recent-recipe-img"></div>
+	</div>
+</article>
+
+<article class="recent-recipe">
+	<div class="recent-recipe-block">
+		<div class="recent-recipe-article">
+			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+			<div class="cellar-info-module">Beer | Brewer</div>
+		</div>
+		<div class="recent-recipe-img"></div>
+	</div>
+</article>
+
+<article class="recent-recipe">
+	<div class="recent-recipe-block">
+		<div class="recent-recipe-article">
+			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+			<div class="cellar-info-module">Beer | Brewer</div>
+		</div>
+		<div class="recent-recipe-img"></div>
+	</div>
+</article>
+
+
+
+<?php get_footer(); ?>
