f1c34dd81fb5bc002af450c09d58ce25aa6ec20b
kylevalenzuela <valenzuela.kyle@gmail.com>
2014-05-27 23:33:28 -0700

    media queries to home page

--- home.php
+++ home.php
@@ -8,71 +8,119 @@
 
 get_header(); ?>
 
+<div class="two-thirds">
 
-<article class="main-recipe">
-	<div class="main-recipe-img">
-		<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
+	<div class="home-top-articles">
+
+		<article class="main-recipe">
+			<div class="main-recipe-img">
+				<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
+			</div>
+			<div class="main-recipe-block">
+				<h5>Lorem Ipsum</h5>
+				<div class="cellar-info-module">Beer | Brewer</div>
+			</div>
+		</article>	
+
+		<article class="recent-recipe">
+			<div class="recent-recipe-block">
+				<div class="recent-recipe-article">
+					<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+					<div class="cellar-info-module">Beer | Brewer</div>
+				</div>
+				<div class="recent-recipe-img">
+					<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
+				</div>
+			</div>
+
+
+			<div class="recent-recipe-block">
+				<div class="recent-recipe-article">
+					<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+					<div class="cellar-info-module">Beer | Brewer</div>
+				</div>
+				<div class="recent-recipe-img">
+					<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
+				</div>
+			</div>
+
+			<div class="recent-recipe-block">
+				<div class="recent-recipe-article">
+					<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
+					<div class="cellar-info-module">Beer | Brewer</div>
+				</div>
+				<div class="recent-recipe-img">
+					<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
+				</div>
+			</div>
+		</article><!--recent-recipe-->
 	</div>
-	<div class="main-recipe-block">
-		<h5>Lorem Ipsum</h5>
-		<div class="cellar-info-module">Beer | Brewer</div>
-	</div>
-</article>	
 
-<article class="recent-recipe">
-	<div class="recent-recipe-block">
-		<div class="recent-recipe-article">
-			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
-			<div class="cellar-info-module">Beer | Brewer</div>
+	<div class="home-cellar">
+		<h1>The Cellar</h1>
+		
+		<div class="scroll-border">
+	    	<h3>swipe</h3>
+	    	<div class="scroll-animation"></div>
+	 	</div>
+		<div class="home-cellar-outer-container">
+			<div class="home-cellar-container">
+				<div class="home-cellar-carousel" style="background:red;">
+
+					<div class="home-cellar-content">
+						<div class="home-cellar-img">
+							<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/bottle.png" alt="">
+						</div>
+						<div class="home-cellar-block">
+							<h2>Red Trolley Ale</h2>
+							<h3>karl Strauss</h3>
+						</div>
+					</div>
+
+					<div class="home-cellar-content">
+						<div class="home-cellar-img">
+							<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/bottle.png" alt="">
+						</div>
+						<div class="home-cellar-block">
+							<h2>Red Trolly Ale</h2>
+							<h3>karl Strauss</h3>
+						</div>
+					</div>
+
+					<div class="home-cellar-content">
+						<div class="home-cellar-img">
+							<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/bottle.png" alt="">
+						</div>
+						<div class="home-cellar-block">
+							<h2>Red Trolly Ale</h2>
+							<h3>karl Strauss</h3>
+						</div>
+					</div>
+
+					<div class="home-cellar-content">
+						<div class="home-cellar-img">
+							<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/bottle.png" alt="">
+						</div>
+						<div class="home-cellar-block">
+							<h2>Red Trolly Ale</h2>
+							<h3>karl Strauss</h3>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="recent-recipe-img">
-			<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
-		</div>
-	</div>
+	</div><!--Home Cellar-->
 
+	<article class="beer"> 
 
-	<div class="recent-recipe-block">
-		<div class="recent-recipe-article">
-			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
-			<div class="cellar-info-module">Beer | Brewer</div>
-		</div>
-		<div class="recent-recipe-img">
-			<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
-		</div>
-	</div>
+	</article>
+</div><!--two-thirds width-->
 
-	<div class="recent-recipe-block">
-		<div class="recent-recipe-article">
-			<h5>Bacon ipsum dolor sit amet meatball rump tri-tip</h5>
-			<div class="cellar-info-module">Beer | Brewer</div>
-		</div>
-		<div class="recent-recipe-img">
-			<img src="http://www.poop.bakingbrew.com/baked-goods/uploads/2014/05/IMG_20140426_154227.jpg" alt="Feature Article Image">
-		</div>
-	</div>
-
-</article><!--recent-recipe-->
-
-<div class="home-cellar">
-	<h1>The Cellar</h1>
-	<div class="home-cellar-slider">
-		<div class="content">
-	        <div class="page"></div>
-	        <div class="page" style="color;blue;"></div>
-	        <div class="page"></div>
-	        <div class="page"></div>
-		</div>
-	</div>
-</div>
+<aside class="one-third">
+	
+</aside>
 
 
 
-
-
-<script type="text/javascript">
-	$(function() {
-		$('home-cellar-slide-block').pagesSliderTouch();
-	});
-</script>
 
 <?php get_footer(); ?>
