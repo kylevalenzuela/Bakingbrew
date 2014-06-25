cfe79e397c3e46549237a652a40cf35c274e4c59
kylevalenzuela <valenzuela.kyle@gmail.com>
2014-06-24 22:06:35 -0700

    ajax sidebar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+//adding .touch .no-touch classes
 document.documentElement.className +=
     (("ontouchstart" in document.documentElement) ? ' touch' : ' no-touch');
 
+$('').click(function(){
+        $.ajax('recent-popular-post.php', {
+            url: this.href,
+            type: 'GET',
+            dataType: 'html',
+            success: function (data) {
+                $('#container').html(data);
+            }
+        });
+    });
